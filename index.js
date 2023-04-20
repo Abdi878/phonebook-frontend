f7390bf1d989446cb851aeba8430097d31fa80cb
Abdi878 <k21071276@kcl.ac.uk>
2023-04-20 03:31:49 +0100

    oh_yeah

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const morgan = require("morgan");
 const app = express();
 const Person = require("./models/person");
+const mongoose = require("mongoose");
 app.use(express.json());
 morgan.token("body", function (req, res) {
   return JSON.stringify(req.body);
@@ -23,28 +24,6 @@ app.use(
     ].join(" ");
   })
 );
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
 app.get("/", (req, res) => {
   res.send("<h1>GANG<h1>");
 });
@@ -66,14 +45,10 @@ app.get("/api/info", (req, res) => {
   );
 });
 
-app.get("/api/persons/:id", (req, res) => {
-  const id = req.params.id;
-  const person = persons.find((person) => person.id === Number(id));
-  if (person) {
-    res.json(person);
-  } else {
-    res.status(404).end();
-  }
+app.get("/api/persons/:id", (request, response) => {
+  Person.findById(request.params.id).then((person) => {
+    response.json(person);
+  });
 });
 app.delete("/api/persons/:id", (req, res) => {
   const id = req.params.id;
@@ -90,21 +65,12 @@ app.post("/api/persons", (req, res) => {
       error: "content missing",
     });
   }
-  const names = persons.map((person) => person.name);
-  if (names.includes(body.name)) {
-    return res.status(400).json({
-      error: "name must be unique",
-    });
-  }
-
-  const person = {
+  const person = new Person({
     id: generateID(),
     name: body.name,
     number: body.number,
-  };
+  });
   person.save().then((savedPerson) => {
     res.json(savedPerson);
   });
-  console.log(person);
-  persons = persons.concat(person);
 });
